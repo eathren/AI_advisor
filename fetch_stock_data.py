7090529de1ec3a498f24629b13d1d7b25db39843
Nolan <nolan.braman@gmail.com>
2021-11-05 15:09:34 -0400

    fetch a single stock data from AA api, then save file to pandas

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -3,10 +3,11 @@ import requests
 from dotenv import load_dotenv
 import csv
 import random
+import pandas as pd
 
 load_dotenv()
 
-AA_key = os.getenv('AA_key')
+AA_KEY = os.getenv('AA_KEY')
 
 """
 How this is going to work:
@@ -23,39 +24,58 @@ http://www.fmlabs.com/reference/default.htm?url=SimpleMA.htm
 
 
 class StockData:
-    def __init__(self):
-        self.key = os.getenv('AA_key')
-        self.data = []
+    def __init__(self, id=None):
+        self.id = id
         self.history = []
+        self.json = self.fetch_json()
+        self.data = self.fetch_stock()
 
-    def fetch_data(self):
-        # Currently using nasdaq_ids.csv
-        with open('data/nasdaq_ids.csv') as csv_file:
-            csv_data = csv.reader(csv_file)
+    def fetch_json(self):
+        params = {'symbol': self.id, 'apikey': AA_KEY}
+        response = requests.get(
+            "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED",
+            params=params)
 
-            for row in csv_data:
-                self.data.append(row)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"An error has occured with fetching the json data for {self.id}")
 
-    def fetch_live_data(self):
-        for row in self.data[1:2]:
-            params = {'symbol': str(row[0]), 'apikey': str(AA_key)}
-            print(row[0], AA_key)
-            response = requests.get(
-                "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED",
-                params=params)
+    def fetch_stock(self):
+        data = pd.json_normalize(self.json)
+        print(data)
+        return data
 
-            if response.status_code == 200:
-                id_data = response.json()
-                self.history.append(id_data)
+    # def fetch_all_names(self):
+    #     # Currently using nasdaq_ids.csv
+    #     with open('data/nasdaq_ids.csv') as csv_file:
+    #         csv_data = csv.reader(csv_file)
+    #         for row in csv_data:
+    #             self.data.append(row)
 
-    def fetch_random(self):
-        # returns a random stock
+    # def fetch_live_data(self):
+    #     for row in self.data[1:2]:
+    #         params = {'symbol': str(row[0]), 'apikey': str(AA_KEY)}
+    #         print(row[0], AA_KEY)
+    #         response = requests.get(
+    #             "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED",
+    #             params=params)
+    #
+    #         if response.status_code == 200:
+    #             id_data = response.json()
+    #             self.history.append(id_data)
+
+    def print_random(self):
+        """
+        prints a random stock
+        """
         print(self.data[random.randint(2, len(self.data) - 1)])
 
 
 if __name__ == '__main__':
-    data = StockData()
-    data.fetch_data()
-    data.fetch_live_data()
-    print(data.history)
-    data.fetch_random()
+    stock = StockData("AMZN")
+    # data.fetch_all_names()
+    # stock.fetch_live_data()
+    # print(stock.history)
+    # stock.fetch_random()
+    print(stock.json)
--- neural_net.py
+++ neural_net.py
@@ -1,28 +1,57 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import handle_json
+from fetch_stock_data import StockData
 
-# take the data from one stock
-# feed it into the nodes
+"""
+take the data from one stock
+feed it into the nodes
+
+
+WHAT WE WANT:
+Closing stock price of the next day
+
+helpful docs:
+https://blog.quantinsti.com/neural-network-python/
+"""
+
+
+def evaluation_function(actual, predicted):
+    C = np.sum(.5 * ((actual - predicted) ** 2))
 
 
 class NeuralNet:
-    def __init__(self):
+    def __init__(self, id):
+        # If file exists, use those values.
+        # Else, populate as defaults. Save the file every so often.
+        file_path = "data/stocks/" + id + ".json"
+        if handle_json.file_exists(file_path):
+            self.data = pd.read_json()
+            print(self.data)
+            print("YES")
+        else:
+            self.data = StockData(id)
+            print("NO")
         self.learning_rate = 0
         self.historical_success = 0
         self.mentions = 0
         self.curr_trajectory = 0
-        self.cost_per_share = 0 # cost/share
-        self.PE = 0 # P/E
+        self.cost_per_share = 0  # cost/share
+        self.PE = 0  # P/E
         self.age = 0
         self.recently_acquired = False
 
+        # self.C
+
         self.MACD = 0
-        self.trend= 0
+        self.trend = 0
         self.stoch_rsi = 0
         self.rsi = 0
-        self.MA = 0 # Moving average
-
+        self.MA = 0  # Moving average
 
         self.outputs = 0
 
+
+if __name__ == "__main__":
+    net = NeuralNet("AMZN")
