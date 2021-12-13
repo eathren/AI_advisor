1ae9c3581025cef6bc3000e31975572c59d04f14
Nolan <nolan.braman@gmail.com>
2021-12-13 11:11:40 -0500

    added unit tests

--- handle_json.py
+++ handle_json.py
@@ -5,9 +5,10 @@ from os.path import exists as file_exists
 This file handles some logic to check if a file exists, and to write/read json data in those files.
 """
 
+
 def store_json(file, data):
     with open(f"{file}.json", 'w+') as f:
-        json.dump(data, f, indent=4)
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 def load_json(file):
@@ -15,6 +16,5 @@ def load_json(file):
         return json.load(f)
 
 
-def does_file_exist(file):  # Might be a more elegant way to do this one.
+def file_exist(file):  # Might be a more elegant way to do this one.
     return file_exists(file)
-
