5a298d73521af650aabe572132a1fee11b156026
Nolan <nolan.braman@gmail.com>
2021-12-13 12:07:53 -0500

    fix: added abstraction to apis and data fetching

--- apis/alpaca.py
+++ apis/alpaca.py
@@ -4,6 +4,7 @@ import os
 
 import requests
 from alpaca_trade_api.rest import REST, TimeFrame, TimeFrameUnit
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,7 +12,8 @@ AP_KEY = os.getenv('APCA_API_KEY_ID')
 AP_SECRET = os.getenv('APCA_API_SECRET_KEY')
 BASE_URL = os.getenv('APCA_API_BASE_URL')
 
-api = REST(key_id=AP_KEY, secret_key=AP_SECRET, base_url=BASE_URL, api_version="v2")
+api = REST(key_id=AP_KEY, secret_key=AP_SECRET,
+           base_url=BASE_URL, api_version="v2")
 
 
 def get_response(name, time_scale='1D', limit=1000):
